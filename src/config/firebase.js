e0737fe2ef1c825f71050ccdd7457616523289d4
widadrajab14 <widadrajabi146@gmail.com>
2026-05-16 12:12:35 +0300

    firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDXSKuacUxiGkEraG772OCAivOdoftCE6I",
+  authDomain: "matayehuda.firebaseapp.com",
+  projectId: "matayehuda",
+  storageBucket: "matayehuda.firebasestorage.app",
+  messagingSenderId: "264845791661",
+  appId: "1:264845791661:web:bac32332d00b6323671124",
+  measurementId: "G-81XHVPCSNV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
